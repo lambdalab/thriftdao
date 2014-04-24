d8211d77237dd2048f6186cf55928d5903d39d1d
zfy0701 <zfy0701@gmail.com>
2014-04-24 10:30:42 -0700

    Simpliy the bson code by override different fields

--- src/main/java/com/lambdai/thriftdao/bson/LTBSONProtocol.java
+++ src/main/java/com/lambdai/thriftdao/bson/LTBSONProtocol.java
@@ -1,52 +1,47 @@
 package com.lambdai.thriftdao.bson;
 
-import com.foursquare.common.thrift.base.EnhancedTField;
 import com.foursquare.common.thrift.bson.TBSONObjectProtocol;
 import com.foursquare.common.thrift.bson.TBSONProtocol;
 import org.apache.thrift.TException;
-import org.apache.thrift.protocol.TField;
-import org.apache.thrift.protocol.TProtocol;
-import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.protocol.*;
 import org.apache.thrift.transport.TTransport;
 
+import java.lang.reflect.Field;
+
 public class LTBSONProtocol extends TBSONProtocol {
-    public static class WriterFactory extends TBSONObjectProtocol.WriterFactoryForVanillaBSONObject
-            implements TProtocolFactory {
-        public TProtocol getProtocol(TTransport trans) {
-            return super.doGetProtocol(trans);
-        }
+  public static class WriterFactory extends TBSONProtocol.WriterFactory {
+    protected TBSONObjectProtocol createBlankProtocol(TTransport trans) { return doCreateBlankProtocol(trans); }
+  }
+  public static class ReaderFactory extends TBSONProtocol.ReaderFactory {
+    protected TBSONObjectProtocol createBlankProtocol(TTransport trans) { return doCreateBlankProtocol(trans); }
+  }
+  public static class Factory extends ReaderFactory {}
 
-        protected TBSONObjectProtocol createBlankProtocol(TTransport trans) { return doCreateBlankProtocol(trans); }
+  static TBSONObjectProtocol doCreateBlankProtocol(TTransport trans) {
+      LTBSONProtocol ret = new LTBSONProtocol();
+      try {
+        Field transField = TBSONProtocol.class.getDeclaredField("realTransport");
+        transField.setAccessible(true);
+        transField.set(ret, trans);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    // When pull is already change above logic to:
+//      ret.realTransport = trans;
+      return ret;
+  }
+
+  @Override
+  public TField readFieldBegin() throws TException {
+    TField res = super.readFieldBegin();
+    if (res.name.isEmpty() && res.type == TType.STOP && res.id == 0) { // NO_MORE_FIELDS
+      return res;
     }
+    return new TField(res.name, res.type, Short.parseShort(res.name));
+  }
 
-    public static class ReaderFactory extends TBSONObjectProtocol.ReaderFactory implements TProtocolFactory {
-        public TBSONObjectProtocol getProtocol(TTransport trans) {
-            return doGetProtocol(trans);
-        }
-        protected TBSONObjectProtocol doGetProtocol(TTransport trans) {
-            // Set up the protocol for reading.
-            TBSONObjectProtocol ret = createBlankProtocol(trans);
-            ret.setReadState(new LBSONReadState());
-            return ret;
-        }
-        protected TBSONObjectProtocol createBlankProtocol(TTransport trans) { return doCreateBlankProtocol(trans); }
-    }
-
-    // need this for hive. It uses reflection and expects a #Factory static class that implements TProtocolFactory
-    public static class Factory extends ReaderFactory {}
-
-    protected static TBSONObjectProtocol doCreateBlankProtocol(TTransport trans) {
-        LTBSONProtocol ret = new LTBSONProtocol();
-        ret.realTransport = trans;
-        return ret;
-    }
-
-    @Override
-    public void writeFieldBegin(TField tField) throws TException {
-        // Write Id instead of name
-        writeState.putValue(Short.toString(tField.id));
-        if (tField instanceof EnhancedTField) {
-            enhancedType = ((EnhancedTField)tField).enhancedTypes.get("bson");
-        }
-    }
+  @Override
+  public void writeFieldBegin(TField tField) throws TException {
+    super.writeFieldBegin(new TField(Short.toString(tField.id), tField.type, tField.id));
+  }
 }
