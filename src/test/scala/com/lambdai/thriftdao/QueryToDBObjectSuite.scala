18e09088e83d31b1ef2879c46180157ca1d0afdf
zfy0701 <zfy0701@gmail.com>
2014-04-23 20:37:48 -0700

    fix test

--- src/test/scala/com/lambdai/thriftdao/QueryToDBObjectSuite.scala
+++ src/test/scala/com/lambdai/thriftdao/QueryToDBObjectSuite.scala
@@ -2,15 +2,18 @@ package com.lambdai.thriftdao
 
 import org.scalatest.FunSuite
 
-@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
-class QueryToDBObjectSuite extends FunSuite {
-  // TODO test if the DAO generate the right DBObject for query
-  Schemas(
-    Person -> Schema.create(Person) (
+object TestSchemas extends Schemas {
+  apply(
+    Person -> create(Person) (
       primaryKey = List(_.NameField),
       indexes = List(
         Index("Nation", false, List(_.NationalityField))
       )
     )
   )
+}
+
+@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+class QueryToDBObjectSuite extends FunSuite {
+  // TODO test if the DAO generate the right DBObject for query
 }
