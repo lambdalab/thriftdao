b49ea31eb9361ff5cb692d796d3f9f06960bf91e
zfy0701 <zfy0701@gmail.com>
2014-05-11 13:18:52 -0700

    avoid use scala-reflect

--- src/main/scala/com/lambdai/thriftdao/ReflectionHelper.scala
+++ src/main/scala/com/lambdai/thriftdao/ReflectionHelper.scala
@@ -1,12 +1,19 @@
 package com.lambdai.thriftdao
 
-import scala.reflect.runtime.currentMirror
+//import scala.reflect.runtime.currentMirror
 
 object ReflectionHelper {
+//  def companion[T](obj: Any): T = {
+//    val classSymbol = currentMirror.classSymbol(obj.getClass)
+//    val moduleSymbol = classSymbol.companionSymbol.asModule
+//    val moduleMirror = currentMirror.reflectModule(moduleSymbol)
+//    moduleMirror.instance.asInstanceOf[T]
+//  }
+
   def companion[T](obj: Any): T = {
-    val classSymbol = currentMirror.classSymbol(obj.getClass)
-    val moduleSymbol = classSymbol.companionSymbol.asModule
-    val moduleMirror = currentMirror.reflectModule(moduleSymbol)
-    moduleMirror.instance.asInstanceOf[T]
+    val structClass = obj.getClass
+    val companion = Class.forName(structClass.getName + "$")
+    companion.getField("MODULE$").get(null).asInstanceOf[T]
   }
+
 }
